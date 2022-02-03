5a39685e0d6cc85298884657ef7a97e8b87b0558
Nikhil Rana <nikhil.student.eee20@itbhu.ac.in>
2022-02-03 13:53:18 +0530

    Add Error Catch

--- controllers/words.js
+++ controllers/words.js
@@ -1,20 +1,27 @@
 const Words = require('../models/words');
 
 const getWord = async (req, res) => {
-    const word = await Words.findOne({ serial: req.params.serial });
-    if (!word) {
-        return res.status(404).send("Not found!");
-    }
+    try {
+        const word = await Words.findOne({ serial: req.params.serial });
+        if (!word) {
+            return res.status(404).send("Not found!");
+        }
     return res.send(word.word);
+    } catch (error) {
+        res.send(error.message);
+    }
 }
 
 const checkWord = async (req, res) => {
-    console.log(`Its here ${req.params.word}`)
-    let word = await Words.find({ word: req.params.word });
-    if (!word) {
-        return res.status(404).send("Word not found!");
+    try {
+        let word = await Words.findOne({ word: req.params.word });
+        if (!word) {
+            return res.status(404).send("Word not found!");
+        }
+        return res.send(word);    
+    } catch (error) {
+        res.send(error.message);
     }
-    return res.send(word);
 }
 
 module.exports = {
